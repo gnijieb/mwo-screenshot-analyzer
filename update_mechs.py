caccb01fd0ed82dc92e989425f833c08f37294a4
gnijieb@gmail.com <gnijieb@gmail.com>
2016-05-01 17:42:37 +0200

    Added script update_mechs.py which updates the basedata/mechs.txt with current data from smurfy.net using its JSON API The only downside of this is the mechnames on smurfy almost never include any special designations other than (C) or (L). So there could be increased problems when trying to detect mechnames with OCR when they contain designations like (I), (R), (P) and so on.

---
+++ update_mechs.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# This file is part of mwo-screenshot-analyzer.
+#
+# mwo-screenshot-analyzer is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# mwo-screenshot-analyzer is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with mwo-screenshot-analyzer.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright 2016 gnijieb@gmail.com
+
+import sys
+import json
+import urllib
+
+def main():
+	url = "http://mwo.smurfy-net.de/api/data/mechs.json"
+	response = urllib.urlopen(url)
+	data = json.load(response)
+
+	Mechs = []
+	
+	for entry in data.values():
+		Mechs.append(entry["translated_short_name"])
+	Mechs.sort()
+	
+	with open("./basedata/mechs.txt", "w") as myfile:
+		for mech in Mechs:
+			myfile.write(mech+"\n")
+	print("Done")
+	return
+	
+if __name__ == "__main__":
+	main()
